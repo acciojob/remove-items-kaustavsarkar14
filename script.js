fda5e9c6bc7ef67811528e6091017dd72ea1ea40
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-20 12:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function remove(){
+	const colorSelect = document.getElementById('colorSelect')
+	// console.log(colorSelect.value)
+	colorSelect.querySelectorAll('option').forEach(e=>{
+		if(colorSelect.value==e.value) e.remove()
+	})
+}
