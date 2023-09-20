2be22a851091968c494b5263bbc8c6ae7842f5ad
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-20 14:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //your JS code here. If required.
 function remove(){
 	const colorSelect = document.getElementById('colorSelect')
-	// console.log(colorSelect.value)
-	colorSelect.querySelectorAll('option').forEach(e=>{
-		if(colorSelect.value==e.value) e.remove()
-	})
+	console.log(colorSelect.options[colorSelect.selectedIndex])
+	colorSelect.options[colorSelect.selectedIndex].remove()
 }
